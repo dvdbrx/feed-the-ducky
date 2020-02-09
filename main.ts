e94c0bb78cde5b41b767b85f54b14d66d39c666c
dvdbrx <60830662+dvdbrx@users.noreply.github.com>
2020-02-08 16:23:13 -0800

    Ducky shoots lemons to destroy Sharks...

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
+sprites.onCreated(SpriteKind.Projectile, function (sprite) {
+    music.pewPew.play()
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     info.changeScoreBy(1)
-    info.startCountdown(2.4)
+    info.startCountdown(4)
     Strawberry.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 120))
     music.pewPew.playUntilDone()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    Shark.destroy(effects.spray, 500)
+    myEnemy.destroy(effects.spray, 500)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile2 = sprites.createProjectileFromSprite(img`
+4 4 4 . . 4 4 4 4 4 . . . . . . 
+4 5 5 4 4 5 5 5 5 5 4 4 . . . . 
+b 4 5 5 1 5 1 1 1 5 5 5 4 . . . 
+. b 5 5 5 5 1 1 5 5 1 1 5 4 . . 
+. b d 5 5 5 5 5 5 5 5 1 1 5 4 . 
+b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 . 
+c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
+c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4 
+c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4 
+c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4 
+. c 4 5 5 5 5 d d d 5 5 5 5 5 b 
+. c 4 d 5 4 5 d 4 4 d 5 5 5 4 c 
+. . c 4 4 d 4 4 4 4 4 d d 5 d c 
+. . . c 4 4 4 4 4 4 4 4 5 5 5 4 
+. . . . c c b 4 4 4 b b 4 5 4 4 
+. . . . . . c c c c c c b b 4 . 
+`, Ducky, Math.randomRange(-100, 100), Math.randomRange(-100, 100))
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     music.siren.stop()
@@ -16,6 +43,10 @@ info.onCountdownEnd(function () {
 info.onLifeZero(function () {
     game.over(false, effects.slash)
 })
+let projectile2: Sprite = null
+let myEnemy: Sprite = null
+let Shark: Sprite = null
+let Ducky: Sprite = null
 let Strawberry: Sprite = null
 music.baDing.play()
 info.setLife(3)
@@ -38,7 +69,7 @@ e e e 2 e e c e c c c . . . . .
 . c c c c c c c . . . . . . . . 
 `, SpriteKind.Food)
 scene.setBackgroundColor(9)
-let Ducky = sprites.create(img`
+Ducky = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . b 5 5 b . . . 
@@ -56,7 +87,8 @@ c b d d d d d 5 5 5 5 5 5 5 b .
 . . c b d d d d d 5 5 5 b b . . 
 . . . c c c c c c c c b b . . . 
 `, SpriteKind.Player)
-let Shark = sprites.create(img`
+Ducky.setFlag(SpriteFlag.StayInScreen, true)
+Shark = sprites.create(img`
 . . . . . . . . . . . . . c c f f f . . . . . . . . . . . . . . 
 . . . . . . . . . . . . c d d b b f . . . . . . . . . . . . . . 
 . . . . . . . . . . . c d d b b f . . . . . . . . . . . . . . . 
@@ -78,3 +110,25 @@ Shark.setVelocity(30, 30)
 Shark.setPosition(Math.randomRange(10, 160), Math.randomRange(10, 20))
 Shark.follow(Ducky, 10)
 controller.moveSprite(Ducky)
+game.onUpdateInterval(5000, function () {
+    myEnemy = sprites.create(img`
+. . . . . . . . . . . . . c c f f f . . . . . . . . . . . . . . 
+. . . . . . . . . . . . c d d b b f . . . . . . . . . . . . . . 
+. . . . . . . . . . . c d d b b f . . . . . . . . . . . . . . . 
+. . . . . . . . . . f c c b b c f . . . . . . . . . . . . c c c 
+. . . . f f f f f f c c c c c c f f . . . . . . . . . c c b b c 
+. . f f b b b b b b b b b b b b b c f f f . . . . . c d b b c . 
+f f b b b b b b b b b c b c b b b b c c c f f . . c d d b b f . 
+f b c b b b b b f f b b c b c b b b c c c c c f f f d b b f . . 
+f b b b 1 1 1 1 f f 1 b c b c b b b c c c c c c c b b b c f . . 
+. f b 1 1 1 1 1 1 1 1 b b b b b b c c c c c c c c c b c c f . . 
+. . f c c c 3 3 c c 1 1 b b b b c c c c c c c c f f f b b c f . 
+. . . f c 1 3 1 c 1 1 1 b b b c c c c c b d b c . . . f b b f . 
+. . . . f 3 3 c 1 1 1 c b b b f d d d d d c c . . . . . f b b f 
+. . . . . f f 1 1 1 1 f b d b b f d d c c . . . . . . . . f f f 
+. . . . . . . c c c c c f b d b b f c . . . . . . . . . . . . . 
+. . . . . . . . . . . . . f f f f f . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+    myEnemy.setPosition(10, 10)
+    myEnemy.follow(Ducky, 10)
+})
